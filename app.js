ca6049eb6e81925656827d99f8c04743c9682a65
Nick Savaliya <nikunj.savaliya@fitterfly.in>
2025-04-30 17:54:48 +0530

    socket with user connection

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import { connectDB } from './config/db.js';
 import routes from './app/routes/index.js';
 import cookieParser from 'cookie-parser';
 const app = express();
-app.use(cors());
+app.use(cors('*'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- socket-server.js
+++ socket-server.js
@@ -4,9 +4,18 @@ import app from './app.js';
 const httpServer = createServer(app);   
 
 const io = new Server(httpServer);
+
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    let {user_id} = socket.handshake.query;
+    
+    console.log('a user connected: ', user_id);
+    socket.on('message', (msg) => {
+        socket.emit('message', msg);
+        socket.broadcast.emit('message', msg);
+    });
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        console.log(socket.id +' user disconnected');
     });
 });
+
+export default httpServer;
